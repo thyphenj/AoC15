4dadcb21fd236054934f669d96f3b3756645dbb7
t-j <t-j@terence-john.co.uk>
2022-08-26 17:33:18 +0100

    day 1 complete

---
+++ AoC15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Aoc1501
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brackets = File.ReadAllText("input.txt");
+
+            int floor = 0;
+            int charpos = 0;
+            int basement = 0;
+            bool beenToBasement = false;
+
+            foreach ( var ch in brackets)
+            {
+                charpos++;
+                if (ch == '(')
+                    floor++;
+                if ( ch == ')')
+                    floor--;
+                if (floor == -1 && !beenToBasement)
+                {
+                    beenToBasement = true;
+                    basement = charpos;
+                }
+            }
+            Console.WriteLine($"part 1 - {floor}");
+            Console.WriteLine($"part 2 - {basement}");
+        }
+    }
+}
