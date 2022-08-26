e5f3bbbc160738d7ea35512c66bab149c5d47ac0
t-j <t-j@terence-john.co.uk>
2022-08-26 18:47:21 +0100

    day 3 past 1 complete

---
+++ AoC1503/Point.cs
@@ -0,0 +1,18 @@
+﻿namespace Aoc1503
+{
+    internal class Position
+    {
+        private int X;
+        private int Y;
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return $"{X},{Y}";
+        }
+    }
+}
---
+++ AoC1503/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Aoc1503
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            HashSet<string> positions = new HashSet<string>();
+
+            int x = 0;
+            int y = 0;
+
+            positions.Add($"{x},{y}");
+
+            foreach ( var ch in input)
+            {
+                switch (ch)
+                {
+                    case '>':
+                        x++;
+                        break;
+                    case '<':
+                        x--;
+                        break;
+                    case '^' :
+                        y++;
+                        break;
+                    case 'v':
+                        y--;
+                        break;
+                    default:
+                        break;
+                }
+                positions.Add($"{x},{y}");
+            }
+            Console.WriteLine(positions.Count);
+        }
+    }
+}
