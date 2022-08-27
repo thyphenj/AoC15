1f86e6654e3071ebd3cb43efb3e08a64f3501375
t-j <t-j@terence-john.co.uk>
2022-08-27 11:24:06 +0100

    day 5 part 1

---
+++ AoC1504/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Aoc1504
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "ckczppom";
+            //string key = "abcdef";
+            string theHash = "";
+            long i = 1;
+
+            while (true)
+            {
+                using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes($"{key}{i}");
+                    byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                    theHash = Convert.ToHexString(hashBytes);
+                    if (theHash.Substring(0,6) == "000000") break;
+
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"{i}  {theHash}");
+        }
+    }
+}
---
+++ AoC1505/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Aoc1505
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int niceCount = 0;
+            foreach (var str in input)
+            {
+                if (str.Contains("ab")) continue;
+                if (str.Contains("cd")) continue;
+                if (str.Contains("pq")) continue;
+                if (str.Contains("xy")) continue;
+
+                int vowelCount = 0;
+                bool repeatedChar = false;
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ("aeiou".Contains(str[i]))
+                        vowelCount++;
+                    if (i + 1 < str.Length && str[i] == str[i + 1])
+                        repeatedChar = true;
+                }
+                if (vowelCount > 2 && repeatedChar)
+                    niceCount++;
+            }
+            Console.WriteLine(niceCount);
+        }
+    }
+}
