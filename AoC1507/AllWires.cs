44a5ae4122b8bcfb0ae033aed63a3dc45430ae4f
t-j <t-j@terence-john.co.uk>
2022-08-27 17:00:54 +0100

    Day 6 refactored and Complete

---
+++ AoC1507/AllWires.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace AoC1507
+{
+    public class AllWires
+    {
+        public Dictionary<string, Wire> Wires = new Dictionary<string, Wire>();
+
+        public AllWires()
+        {
+        }
+
+        public void Add(string line)
+        {
+            string[] tok = line.Split(' ');
+
+            switch (tok.Length)
+            {
+                case 3:
+                    uint value = 0;
+                    if (uint.TryParse(tok[0], out value))
+                        Wires.Add(tok[2], new Wire(value));
+                    else
+                        Wires.Add(tok[2], new Wire(tok[0]));
+                    break;
+
+                case 4:
+                    Wires.Add(tok[3], new Wire(tok[0], tok[1]));
+                    break;
+
+                case 5:
+                    Wires.Add(tok[4], new Wire(tok[0], tok[1], tok[2]));
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public void Add(string key, Wire wire)
+        {
+            Wires.Add(key, wire);
+        }
+
+        public uint Resolve(string key)
+        {
+            Wire currWire = Wires[key];
+
+            uint retval = 0;
+
+            if (currWire.Value is not null)
+                retval = (uint)currWire.Value;
+
+            else if (currWire.Source == SignalSource.Wire)
+                retval = Resolve(currWire.OpOne);
+
+            else if (currWire.Source == SignalSource.Gate)
+            {
+                var opLeft = currWire.OpOneValue ?? Resolve(currWire.OpOne);
+
+                if (currWire.Gate == "NOT")
+                    retval = ~opLeft;
+
+                else if (currWire.Gate == "LSHIFT")
+                    retval = opLeft << int.Parse(currWire.OpTwo);
+
+                else if (currWire.Gate == "RSHIFT")
+                    retval = opLeft >> int.Parse(currWire.OpTwo);
+
+                else
+                {
+                    var opRite = currWire.OpTwoValue ?? Resolve(currWire.OpTwo);
+
+                    if (currWire.Gate == "AND")
+                        retval = opLeft & opRite;
+
+                    if (currWire.Gate == "OR")
+                        retval = opLeft | opRite;
+                }
+            }
+
+            currWire.Value = retval;
+
+            return retval;
+        }
+    }
+}
+
--- AoC1507/Program.cs
+++ AoC1507/Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         static Dictionary<string, Wire> wires = new Dictionary<string, Wire>();
+        static int Depth = 0;
 
         static void Main(string[] args)
         {
@@ -10,79 +11,29 @@
             //string filename = "input-test.txt";
             string[] input = File.ReadAllLines(filename);
 
+            var wires1 = new AllWires();
+            var wires2 = new AllWires();
+
             foreach (var line in input)
             {
-                string[] tok = line.Split(' ');
-
-                switch (tok.Length)
-                {
-                    case 3:
-                        uint value = 0;
-                        if (uint.TryParse(tok[0], out value))
-                            wires.Add(tok[2], new Wire(value));
-                        else
-                            wires.Add(tok[2], new Wire(tok[0]));
-                        break;
-
-                    case 4:
-                        wires.Add(tok[3], new Wire(tok[0], tok[1]));
-                        break;
-
-                    case 5:
-                        wires.Add(tok[4], new Wire(tok[0], tok[1], tok[2]));
-                        break;
-
-                    default:
-                        break;
-                }
-            }
-            //foreach ( var wire in wires)
-            //    Console.WriteLine($"{wire.Key,2}    {wire.Value}");
-
-            uint answer = Resolve("a",wires["a"]);
-
-            Console.WriteLine(answer);
-        }
-
-        internal static uint Resolve(string key,Wire root)
-        {
-            Console.WriteLine($"{key,2}   {root}");
-            uint retval = 0;
-
-            if (root.Value is not null)
-                retval =  (uint)root.Value;
-
-            else if (root.Source == SignalSource.Wire)
-                retval = Resolve(root.OpOne,wires[root.OpOne]);
-
-            else if (root.Source == SignalSource.Gate)
-            {
-                var opLeft =  root.OpOneValue ??  Resolve(root.OpOne, wires[root.OpOne]);
-
-                if (root.Gate == "NOT")
-                    retval = ~opLeft;
-
-                else if (root.Gate == "LSHIFT")
-                    retval =  opLeft << int.Parse(root.OpTwo);
-
-                else if (root.Gate == "RSHIFT")
-                    retval =  opLeft >> int.Parse(root.OpTwo);
-
-                else
-                {
-                    var opRite = root.OpTwoValue ?? Resolve(root.OpTwo,wires[root.OpTwo]);
-
-                    if (root.Gate == "AND")
-                        retval = opLeft & opRite;
-
-                    if (root.Gate == "OR")
-                        retval =  opLeft | opRite;
-                }
+                wires1.Add(line);
+                wires2.Add(line);
             }
 
-            root.Value = retval;
+            // -- Part 1
 
-            return retval;
+            uint answer1 = wires1.Resolve("a");
+
+            // -- Part 2
+
+            wires2.Wires["b"].Value = answer1;
+
+            uint answer2 = wires2.Resolve("a");
+
+
+            Console.WriteLine($"Part 1 - {answer1}");
+            Console.WriteLine($"Part 2 - {answer2}");
         }
+
     }
 }
