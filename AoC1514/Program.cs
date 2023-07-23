2593221e6f226b8b029b5dfe3e1879518feb4628
t-j <t-j@terence-john.co.uk>
2023-07-23 18:51:23 +0100

    XIV i complete

---
+++ AoC1514/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace AoC1514
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "input.txt";
+            //string filename = "input-test.txt";
+
+            var reindeers = new List<Reindeer>();
+
+            var lines = File.ReadAllLines(filename);
+
+            foreach (var line in lines)
+            {
+                //Console.WriteLine(line);
+                var tokens = line.Split(' ');
+                reindeers.Add(new Reindeer(tokens[0], tokens[3], tokens[6], tokens[13]));
+            }
+
+            int maxDistance = 0;
+            foreach ( var r in reindeers)
+            {
+                int d = r.Distance(2503);
+                Console.WriteLine( $"{r.Name.PadRight(8)} - {d,6}");
+                if (d > maxDistance)
+                    maxDistance = d;
+            }
+            Console.WriteLine($"Part 1 - {maxDistance}");
+        }
+    }
+}
---
+++ AoC1514/Reindeer.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC1514
+{
+    public class Reindeer
+    {
+        public string Name;
+        public int Speed;
+        public int Flight;
+        public int Rest;
+
+        public Reindeer(string a, string b, string c, string d)
+        {
+            Name = a;
+            Speed = int.Parse(b);
+            Flight = int.Parse(c);
+            Rest = int.Parse(d);
+        }
+
+        public int Distance(int sec)
+        {
+            int cycles = sec / (Flight + Rest);
+            int extra = sec % (Flight + Rest);
+
+            int dist = cycles * Flight * Speed;
+
+            if (extra < Flight)
+                dist += extra * Speed;
+            else
+                dist += Flight * Speed;
+
+            return dist;
+        }
+    }
+}
+
