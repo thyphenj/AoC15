3064b92bbe5daefef81ac90f9f4c75b0a6e5c0d8
t-j <t-j@terence-john.co.uk>
2023-07-23 19:06:56 +0100

    XIV ii complete

--- AoC1514/Program.cs
+++ AoC1514/Program.cs
@@ -19,15 +19,42 @@ namespace AoC1514
                 reindeers.Add(new Reindeer(tokens[0], tokens[3], tokens[6], tokens[13]));
             }
 
+            // ---- Part 1 : Who's gone the furthest
             int maxDistance = 0;
-            foreach ( var r in reindeers)
+            foreach (var r in reindeers)
             {
                 int d = r.Distance(2503);
-                Console.WriteLine( $"{r.Name.PadRight(8)} - {d,6}");
+                //Console.WriteLine( $"{r.Name.PadRight(8)} - {d,6}");
                 if (d > maxDistance)
                     maxDistance = d;
             }
             Console.WriteLine($"Part 1 - {maxDistance}");
+
+            // ---- Part 2 : Who's been most often in the lead
+            for (int t = 1; t <= 2503; t++)
+            {
+                maxDistance = 0;
+                var leader = reindeers[0];
+
+                foreach (var r in reindeers)
+                {
+                    int d = r.Distance(t);
+                    if (d > maxDistance)
+                    {
+                        maxDistance = d;
+                        leader = r;
+                    }
+                }
+                leader.Points++;
+            }
+            int maxPoints = 0;
+            foreach (var r in reindeers)
+            {
+                if (r.Points > maxPoints)
+                    maxPoints = r.Points;
+                //Console.WriteLine( $"{r.Name.PadRight(8)} - {r.Points,6}");
+            }
+            Console.WriteLine($"Part 2 - {maxPoints}");
         }
     }
 }
--- AoC1514/Reindeer.cs
+++ AoC1514/Reindeer.cs
@@ -7,12 +7,18 @@
         public int Flight;
         public int Rest;
 
+        public int Position;
+
+        public int Points;
+
         public Reindeer(string a, string b, string c, string d)
         {
             Name = a;
             Speed = int.Parse(b);
             Flight = int.Parse(c);
             Rest = int.Parse(d);
+
+            Points = 0;
         }
 
         public int Distance(int sec)
@@ -26,6 +32,8 @@
                 dist += extra * Speed;
             else
                 dist += Flight * Speed;
+
+            Position = dist;
 
             return dist;
         }
