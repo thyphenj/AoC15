34746acdc1ab88d15672a6822bd158260a532891
t-j <t-j@terence-john.co.uk>
2023-07-25 15:09:35 +0100

    XVI i complete

---
+++ AoC1516/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> detected = new Dictionary<string, int>();
+
+        detected.Add("children", 3);
+        detected.Add("cats", 7);
+        detected.Add("samoyeds", 2);
+        detected.Add("pomeranians", 3);
+        detected.Add("akitas", 0);
+        detected.Add("vizslas", 0);
+        detected.Add("goldfish", 5);
+        detected.Add("trees", 3);
+        detected.Add("cars", 2);
+        detected.Add("perfumes", 1);
+
+        string filename = "input.txt";
+
+        string[] lines = File.ReadAllLines(filename);
+
+        Sue[] sues = new Sue[lines.Length];
+
+        foreach (var line in lines)
+        {
+            string localLine = line.Substring(4);
+            int i = localLine.IndexOf(':');
+
+            int sueNumber = int.Parse(localLine.Substring(0, i)) - 1;
+            sues[sueNumber] = new Sue(localLine.Substring(i + 2));
+        }
+
+        Dictionary<string, int> found = new Dictionary<string, int>();
+
+        for (int i = 0; i < sues.Length; i++)
+        {
+            var sue = sues[i];
+            bool gotOne = true;
+            foreach (var e in sue.Elements)
+            {
+                if (e.Value != detected[e.Key])
+                {
+                    gotOne = false;
+                    break; ;
+                }
+            }
+            if (gotOne)
+                Console.WriteLine($"{i + 1,3} - {sue}");
+        }
+    }
+}
---
+++ AoC1516/Sue.cs
@@ -0,0 +1,27 @@
+﻿public class Sue
+{
+    public Dictionary<string,int> Elements;
+
+    public Sue(string line)
+    {
+        Elements = new Dictionary<string,int>();
+
+        string[] toks = line.Split(", ");
+        foreach ( var tok in toks)
+        {
+            var thing = tok.Split(": ");
+            Elements.Add(thing[0], int.Parse(thing[1]));
+        }
+    }
+
+    public override string ToString()
+    {
+        string retval = "";
+        foreach ( var e in Elements)
+        {
+            retval += $", {e.Key.PadRight(11)}: {e.Value,4}" ;
+        }
+        return retval.Substring(2);
+    }
+}
+
