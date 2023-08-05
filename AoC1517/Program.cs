0e57c0c6dd7b4e059e937fcd82dc5d2fbfdf3a2b
t-j <t-j@terence-john.co.uk>
2023-08-05 14:32:42 +0100

    Day 18 completed

---
+++ AoC1517/Program.cs
@@ -0,0 +1,254 @@
+﻿class Program
+{
+    static bool testing = true;
+    static string Filename = "input.txt";
+    static int Target = 150;
+
+    static int[] capacity = { };
+    static int[] contInd = { };
+
+    static void Main()
+    {      
+        if (testing)
+        {
+            Filename = "input-test.txt";
+            Target = 25;
+        }
+        string[] lines = File.ReadAllLines(Filename);
+        int noofContainers = lines.Length;
+
+        capacity = new int[noofContainers];
+        contInd = new int[noofContainers];
+
+        for (int i = 0; i < noofContainers; i++)
+        {
+            capacity[i] = int.Parse(lines[i]);
+        }
+        capacity = capacity.Order().ToArray<int>();
+
+        int noofCombinations = 0;
+        int two = 0;
+        int four = 0;
+        int sum = 0;
+        int knd = 0;
+        for (contInd[knd] = 0; contInd[knd] < noofContainers; contInd[knd]++)
+        {
+            if (sum + capacity[contInd[knd]] > Target) continue;
+            else if (sum + capacity[contInd[knd]] == Target)
+            {
+                if ((++noofCombinations) % 10 == 1)
+                    Console.WriteLine();
+                Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                for (int i = 0; i <= knd; i++)
+                    Console.Write($" {capacity[contInd[i]],3}");
+                Console.WriteLine();
+                break;
+            }
+
+
+
+            sum += capacity[contInd[knd++]];
+            for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+            {
+                if (sum + capacity[contInd[knd]] > Target) continue;
+                else if (sum + capacity[contInd[knd]] == Target)
+                {
+                    if ((++noofCombinations) % 10 == 1)
+                        Console.WriteLine();
+                    Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                    for (int i = 0; i <= knd; i++)
+                        Console.Write($" {capacity[contInd[i]],3}");
+                    Console.WriteLine($"  ------------------- {++two}");
+
+                    break;
+                }
+
+                sum += capacity[contInd[knd++]];
+                for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                {
+                    if (sum + capacity[contInd[knd]] > Target) continue;
+                    else if (sum + capacity[contInd[knd]] == Target)
+                    {
+                        if ((++noofCombinations) % 10 == 1)
+                            Console.WriteLine();
+                        Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                        for (int i = 0; i <= knd; i++)
+                            Console.Write($" {capacity[contInd[i]],3}");
+                        Console.WriteLine();
+                        break;
+                    }
+
+                    sum += capacity[contInd[knd++]];
+                    for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                    {
+                        if (sum + capacity[contInd[knd]] > Target) continue;
+                        else if (sum + capacity[contInd[knd]] == Target)
+                        {
+                            if ( (++noofCombinations) % 10 == 1)
+                                Console.WriteLine();
+                            Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                            for (int i = 0; i <= knd; i++)
+                                Console.Write($" {capacity[contInd[i]],3}");
+                            Console.WriteLine($"  ------------------- {++four}");
+                            break;
+                        }
+
+                        sum += capacity[contInd[knd++]];
+                        for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                        {
+                            if (sum + capacity[contInd[knd]] > Target) continue;
+                            else if (sum + capacity[contInd[knd]] == Target)
+                            {
+                                if ((++noofCombinations) % 10 == 1)
+                                    Console.WriteLine();
+                                Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                for (int i = 0; i <= knd; i++)
+                                    Console.Write($" {capacity[contInd[i]],3}");
+                                Console.WriteLine();
+                                break;
+                            }
+
+                            sum += capacity[contInd[knd++]];
+                            for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                            {
+                                if (sum + capacity[contInd[knd]] > Target) continue;
+                                else if (sum + capacity[contInd[knd]] == Target)
+                                {
+                                    if ((++noofCombinations) % 10 == 1)
+                                        Console.WriteLine();
+                                    Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                    for (int i = 0; i <= knd; i++)
+                                        Console.Write($" {capacity[contInd[i]],3}");
+                                    Console.WriteLine();
+                                    break;
+                                }
+
+                                sum += capacity[contInd[knd++]];
+                                for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                                {
+                                    if (sum + capacity[contInd[knd]] > Target) continue;
+                                    else if (sum + capacity[contInd[knd]] == Target)
+                                    {
+                                        if ((++noofCombinations) % 10 == 1)
+                                            Console.WriteLine();
+                                        Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                        for (int i = 0; i <= knd; i++)
+                                            Console.Write($" {capacity[contInd[i]],3}");
+                                        Console.WriteLine();
+                                        break;
+                                    }
+
+                                    sum += capacity[contInd[knd++]];
+                                    for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                                    {
+                                        if (sum + capacity[contInd[knd]] > Target) continue;
+                                        else if (sum + capacity[contInd[knd]] == Target)
+                                        {
+                                            if ((++noofCombinations) % 10 == 1)
+                                                Console.WriteLine();
+                                            Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                            for (int i = 0; i <= knd; i++)
+                                                Console.Write($" {capacity[contInd[i]],3}");
+                                            Console.WriteLine();
+                                            break;
+                                        }
+
+                                        sum += capacity[contInd[knd++]];
+                                        for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                                        {
+                                            if (sum + capacity[contInd[knd]] > Target) continue;
+                                            else if (sum + capacity[contInd[knd]] == Target)
+                                            {
+                                                if ((++noofCombinations) % 10 == 1)
+                                                    Console.WriteLine();
+                                                Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                                for (int i = 0; i <= knd; i++)
+                                                    Console.Write($" {capacity[contInd[i]],3}");
+                                                Console.WriteLine();
+                                                break;
+                                            }
+
+                                            sum += capacity[contInd[knd++]];
+                                            for (contInd[knd] = contInd[knd - 1] + 1; contInd[knd] < noofContainers; contInd[knd]++)
+                                            {
+                                                if (sum + capacity[contInd[knd]] > Target) continue;
+                                                else if (sum + capacity[contInd[knd]] == Target)
+                                                {
+                                                    if ((++noofCombinations) - 1 % 10 == 1)
+                                                        Console.WriteLine();
+                                                    Console.Write($"{noofCombinations,3}  -  {sum + capacity[contInd[knd]],3}    ");
+                                                    for (int i = 0; i <= knd; i++)
+                                                        Console.Write($" {capacity[contInd[i]],3}");
+                                                    Console.WriteLine();
+                                                    break;
+                                                }
+
+                                                {
+                                                    /* insert shit here */
+                                                }
+                                            }
+                                            sum -= capacity[contInd[--knd]];
+                                        }
+                                        sum -= capacity[contInd[--knd]];
+                                    }
+                                    sum -= capacity[contInd[--knd]];
+                                }
+                                sum -= capacity[contInd[--knd]];
+                            }
+                            sum -= capacity[contInd[--knd]];
+                        }
+                        sum -= capacity[contInd[--knd]];
+                    }
+                    sum -= capacity[contInd[--knd]];
+                }
+                sum -= capacity[contInd[--knd]];
+            }
+            sum -= capacity[contInd[--knd]];
+        }
+        //for (int i = 0; i < cnt; i++)
+        //{
+        //    Recurse(capacity[..1], capacity[1..]);
+        //    capacity = capacity[1..];
+        //}
+    }
+
+    internal static void Recurse(int[] perm, int[] atoms)
+    {
+        int theSum = perm.Sum();
+        Console.WriteLine($"{theSum,4} - [{show(perm)}]   --   [{show(atoms)}]");
+        if (theSum == Target)
+        {
+            foreach (var a in perm)
+            {
+                //Console.Write($"{a,3}  ");
+            }
+            //Console.WriteLine($"-- {theSum}");
+        }
+        else if (theSum < Target)
+        {
+            Array.Resize<int>(ref perm, perm.Length + 1);
+            foreach (var atom in atoms)
+            {
+                perm[perm.Length - 1] = atom;
+                Recurse(perm, atoms[1..]);
+            }
+            Array.Resize<int>(ref perm, perm.Length - 1);
+        }
+    }
+    private static string show(List<int> lst)
+    {
+        string retval = "";
+
+        foreach (var l in lst)
+            retval += $"  {l,3}";
+        return retval;
+    }
+    private static string show(int[] lst)
+    {
+        string retval = "";
+
+        foreach (var l in lst)
+            retval += $"  {l,3}";
+        return retval;
+    }
+}
---
+++ AoC1518/Program.cs
@@ -0,0 +1,135 @@
+﻿class Program
+{
+    static bool testing = false;
+    static string Filename = "input.txt";
+    static int Steps = 100;
+
+    static string[]? Lines;
+    static bool[,]? Grid;
+    static int GridSize = 0;
+
+    static void Main()
+    {
+        if (testing)
+        {
+            Filename = "input-test.txt";
+            Steps = 4;
+        }
+        Lines = File.ReadAllLines(Filename);
+        GridSize = Lines.Length;
+
+        Grid = new bool[GridSize + 2, GridSize + 2]; // -- default to false
+
+        for (int j = 0; j < GridSize; j++)
+        {
+            int x = j + 1;
+            int i = 0;
+            foreach (var ch in Lines[j])
+            {
+                int y = i + 1;
+                Grid[x, y] = (ch == '#');
+                i++;
+            }
+        }
+        // -- Part 1
+        int count = 0;
+        for (int i = 0; i < Steps; i++)
+        {
+            Grid = Iterate();
+            count = CountGrid();
+        }
+        Console.WriteLine(count);
+
+        Grid = ResetGrid();
+        count = 0;
+        for (int i = 0; i < Steps; i++)
+        {
+            Grid = Iterate(2);
+            count = CountGrid();
+        }
+        Console.WriteLine(count);
+    }
+
+    static bool[,] ResetGrid()
+    {
+        var retval = new bool[GridSize + 2, GridSize + 2];
+
+        int j = 0;
+        foreach (var line in Lines)
+        {
+            int x = j + 1;
+            int i = 0;
+            foreach (var ch in line)
+            {
+                int y = i + 1;
+                retval[x, y] = (ch == '#');
+                i++;
+            }
+            j++;
+        }
+        return retval;
+    }
+
+    static bool[,] Iterate(int part = 1)
+    {
+        var retval = new bool[GridSize + 2, GridSize + 2];
+
+        for (int x = 1; x <= GridSize; x++)
+        {
+            for (int y = 1; y <= GridSize; y++)
+            {
+                int neighbours = 0;
+                foreach (var x_offset in new int[] { -1, 0, +1 })
+                {
+                    foreach (var y_offset in new int[] { -1, 0, +1 })
+                    {
+                        if (x_offset != 0 || y_offset != 0)
+                        {
+                            if (Grid![x + x_offset, y + y_offset])
+                                neighbours++;
+                        }
+                    }
+                }
+                if (Grid[x, y])
+                {
+                    retval[x, y] = (neighbours == 2 || neighbours == 3);
+                }
+                else
+                {
+                    retval[x, y] = (neighbours == 3);
+                }
+                if (part != 1)
+                {
+                    if ((x == 1 || x == 100) && (y == 1 || y == 100))
+                        retval[x, y] = true;
+                }
+            }
+        }
+
+        return retval;
+    }
+
+    static void DrawGrid()
+    {
+        for (int x = 1; x <= GridSize; x++)
+        {
+            for (int y = 1; y <= GridSize; y++)
+            {
+                Console.Write(Grid[x, y] ? '#' : '.');
+            }
+            Console.WriteLine();
+        }
+    }
+    static int CountGrid()
+    {
+        int count = 0;
+        for (int x = 1; x <= GridSize; x++)
+        {
+            for (int y = 1; y <= GridSize; y++)
+            {
+                count += Grid[x, y] ? 1 : 0;
+            }
+        }
+        return count;
+    }
+}
