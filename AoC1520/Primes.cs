84938902598ea51e3a8e33abb6f37ad40c0403f1
t-j <t-j@terence-john.co.uk>
2023-08-08 18:27:49 +0100

    completed day XX

---
+++ AoC1520/Primes.cs
@@ -0,0 +1,23 @@
+﻿public static class PrimeTable
+{
+    public static IEnumerable<int> Primes()
+    {
+        return new[] { 2 }.Concat(OddInts().Where(x =>
+        {
+            var sqrt = Math.Sqrt(x);
+            return !OddInts()
+                        .TakeWhile(y => y <= sqrt)
+                        .Any(y => x % y == 0);
+        }));
+    }
+
+    private static IEnumerable<int> OddInts()
+    {
+        int start = 1;
+        while (start > 0)
+        {
+            yield return start += 2;
+        }
+    }
+
+}
---
+++ AoC1520/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int arrSize = 3400000;
+        int[] houses = new int[arrSize];
+        int min = arrSize*10; 
+
+        for (int i = 1; i < arrSize-1; ++i)
+            for (int j = i; j < arrSize && j < min; j += i)
+                if ((houses[j] += i * 10) >= arrSize*10)
+                    min = Math.Min(min, j);
+
+        Console.WriteLine(min);
+
+        houses = new int[arrSize];
+        min = arrSize * 10;
+
+        for (int i = 1; i < arrSize - 1; ++i)
+            for(int j = i,c=0; j < arrSize && j < min&& c < 50; j += i, c++)           
+                if ((houses[j] += i * 11) >= arrSize * 10)
+                    min = Math.Min(min, j);
+
+        Console.WriteLine(min);
+    }
+}
