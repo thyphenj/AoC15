12eafa1563625738d4fd3dedaec25ace09e2db5e
t-j <t-j@terence-john.co.uk>
2023-10-07 12:46:21 +0100

    Completed day 21

---
+++ AoC1521/Item.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC1521;
+
+public class Item
+{
+    public string Name;
+
+    public int Cost;
+    public int Damage;
+    public int Armour;
+
+    public Item(string name)
+    {
+        Name = name;
+    }
+    public override string ToString()
+    {
+        return $"{Name.PadLeft(10)}";
+    }
+}
